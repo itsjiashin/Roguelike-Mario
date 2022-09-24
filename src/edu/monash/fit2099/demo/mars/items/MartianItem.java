7e71b84e619a12b4abc900a3f70bc805c7dffd7e
itsjiashin <100836015+itsjiashin@users.noreply.github.com>
2022-09-25 01:42:32 +0800

    Add files via upload

---
+++ src/edu/monash/fit2099/demo/mars/items/MartianItem.java
@@ -0,0 +1,18 @@
+package edu.monash.fit2099.demo.mars.items;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.items.PickUpItemAction;
+import game.Player;
+
+public class MartianItem extends Item{
+
+	public MartianItem(String name, char displayChar, boolean portable) {
+		super(name, displayChar, portable);
+	}
+
+	public void addSampleAction(Action newAction){
+		this.addAction(newAction);
+	}
+
+}
